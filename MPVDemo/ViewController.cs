9e671ffc2a3e08f49e979bf0be1bc0323e5f737c
kevin <kevinchou.c@gmail.com>
2018-01-07 16:47:38 +0800

    resturect to mpv.cs and libmpv.cs

--- MPVDemo/ViewController.cs
+++ MPVDemo/ViewController.cs
@@ -11,157 +11,20 @@ namespace MPVDemo
 {
     public partial class ViewController : NSViewController
     {
+
+        private mpv mpvPlayer;
+        private string _mediaFilePath;
+
         public ViewController(IntPtr handle) : base(handle)
         {
         }
 
-        private const int MpvFormatString = 1;
-        private IntPtr _mpvHandle;
-        private string _mediaFilePath;
-
-        [DllImport("libmpv.1.25.0.dylib", EntryPoint = "mpv_create")]
-        private static extern IntPtr MpvCreate();
-
-        [DllImport("libmpv.1.25.0.dylib", EntryPoint = "mpv_initialize")]
-        private static extern int MpvInitialize(IntPtr mpvHandle);
-
-        [DllImport("libmpv.1.25.0.dylib", EntryPoint = "mpv_command")]
-        private static extern int MpvCommand(IntPtr mpvHandle, IntPtr strings);
-
-
-        [DllImport("libmpv.1.25.0.dylib", EntryPoint = "mpv_terminate_destroy")]
-        private static extern int MpvTerminateDestroy(IntPtr mpvHandle);
-
-
-        [DllImport("libmpv.1.25.0.dylib", EntryPoint = "mpv_set_option")]
-        private static extern int MpvSetOption(IntPtr mpvHandle, byte[] name, int format, ref long data);
-
-
-        [DllImport("libmpv.1.25.0.dylib", EntryPoint = "mpv_set_option_string")]
-        private static extern int MpvSetOptionString(IntPtr mpvHandle, byte[] name, byte[] value);
-
-
-        [DllImport("libmpv.1.25.0.dylib", EntryPoint = "mpv_get_property")]
-        private static extern int MpvGetPropertystring(IntPtr mpvHandle, byte[] name, int format, ref IntPtr data);
-
-
-        [DllImport("libmpv.1.25.0.dylib", EntryPoint = "mpv_set_property")]
-        private static extern int MpvSetProperty(IntPtr mpvHandle, byte[] name, int format, ref byte[] data);
-
-
-        [DllImport("libmpv.1.25.0.dylib", EntryPoint = "mpv_free")]
-        private static extern int MpvFree(IntPtr data);
-
-
-
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            _mpvHandle = MpvCreate();
+            Debug.WriteLine("Finished load");
+            mpvPlayer = new mpv(VideoView.Handle);
         }
-
-        public void Pause()
-        {
-            if (_mpvHandle == IntPtr.Zero)
-                return;
-
-            Debug.WriteLine("Time Position is {0} ", TimePosition, null);
-
-            if (IsPaused()) {
-                Play();
-            } else {
-                var bytes = GetUtf8Bytes("yes");
-                MpvSetProperty(_mpvHandle, GetUtf8Bytes("pause"), MpvFormatString, ref bytes);
-            }
-        }
-
-        public int TimePosition {
-            get {
-                
-                var lpBuffer = IntPtr.Zero;
-                MpvGetPropertystring(_mpvHandle, GetUtf8Bytes("time-pos"), 4, ref lpBuffer);
-                var time = lpBuffer.ToInt32();
-                Debug.WriteLine("Time Position is {0} duration is {1}", time, Duration, null);
-
-                return time;
-            }
-
-        }
-
-        public int Duration {
-            get {
-                var lpBuffer = IntPtr.Zero;
-                MpvGetPropertystring(_mpvHandle, GetUtf8Bytes("duration"), 4, ref lpBuffer);
-
-                int duration = lpBuffer.ToInt32();
-
-                return duration;
-            }
-        }
-
-        private void Play()
-        {
-            if (_mpvHandle == IntPtr.Zero)
-                return;
-
-            var bytes = GetUtf8Bytes("no");
-            MpvSetProperty(_mpvHandle, GetUtf8Bytes("pause"), MpvFormatString, ref bytes);
-
-        }
-
-        public bool IsPaused()
-        {
-            if (_mpvHandle == IntPtr.Zero)
-                return true;
-
-            var lpBuffer = IntPtr.Zero;
-            MpvGetPropertystring(_mpvHandle, GetUtf8Bytes("pause"), MpvFormatString, ref lpBuffer);
-            var isPaused = Marshal.PtrToStringAnsi(lpBuffer) == "yes";
-            MpvFree(lpBuffer);
-            return isPaused;
-        }
-
-        public void SetTime(double value)
-        {
-            if (_mpvHandle == IntPtr.Zero)
-                return;
-
-            DoMpvCommand("seek", value.ToString(CultureInfo.InvariantCulture), "absolute");
-        }
-
-        private static byte[] GetUtf8Bytes(string s)
-        {
-            return Encoding.UTF8.GetBytes(s + "\0");
-        }
-
-        public static IntPtr AllocateUtf8IntPtrArrayWithSentinel(string[] arr, out IntPtr[] byteArrayPointers)
-        {
-            int numberOfStrings = arr.Length + 1; // add extra element for extra null pointer last (sentinel)
-            byteArrayPointers = new IntPtr[numberOfStrings];
-            IntPtr rootPointer = Marshal.AllocCoTaskMem(IntPtr.Size * numberOfStrings);
-            for (int index = 0; index < arr.Length; index++)
-            {
-                var bytes = GetUtf8Bytes(arr[index]);
-                IntPtr unmanagedPointer = Marshal.AllocHGlobal(bytes.Length);
-                Marshal.Copy(bytes, 0, unmanagedPointer, bytes.Length);
-                byteArrayPointers[index] = unmanagedPointer;
-            }
-            Marshal.Copy(byteArrayPointers, 0, rootPointer, numberOfStrings);
-            return rootPointer;
-        }
-
-        private void DoMpvCommand(params string[] args)
-        {
-            IntPtr[] byteArrayPointers;
-            var mainPtr = AllocateUtf8IntPtrArrayWithSentinel(args, out byteArrayPointers);
-            MpvCommand(_mpvHandle, mainPtr);
-            foreach (var ptr in byteArrayPointers)
-            {
-                Marshal.FreeHGlobal(ptr);
-            }
-            Marshal.FreeHGlobal(mainPtr);
-        }
-
 
         partial void ChooseFile(NSObject sender)
         {
@@ -188,43 +51,14 @@ namespace MPVDemo
 
         partial void Play(NSObject sender)
         {
-            if (_mpvHandle != IntPtr.Zero)
-                MpvTerminateDestroy(_mpvHandle);
+            mpvPlayer.LoadFile(_mediaFilePath);
 
-            _mpvHandle = MpvCreate();
-
-            if (_mpvHandle == IntPtr.Zero) {
-                Debug.WriteLine("Create MPV Failed");
-            } else {
-                Debug.WriteLine("Create MPV {0}", _mediaFilePath, null);
-            }
-
-            int mpvFormatInt64 = 4;
-            var windowId = VideoView.Handle.ToInt64();
-        
-            MpvSetOption(_mpvHandle, GetUtf8Bytes("wid"), mpvFormatInt64, ref windowId);
-          
-            MpvSetOptionString(_mpvHandle, GetUtf8Bytes("log-file"), GetUtf8Bytes("/Users/zhoukaiwen/mpv.log"));
-
-            MpvSetOptionString(_mpvHandle, GetUtf8Bytes("input-media-keys"), GetUtf8Bytes("yes"));
-            MpvSetOptionString(_mpvHandle, GetUtf8Bytes("input-default-bindings"), GetUtf8Bytes("yes"));
-            MpvSetOptionString(_mpvHandle, GetUtf8Bytes("input-cursor"), GetUtf8Bytes("no"));
-            MpvSetOptionString(_mpvHandle, GetUtf8Bytes("input-vo-keyboard"), GetUtf8Bytes("yes"));
-
-            MpvSetOptionString(_mpvHandle, GetUtf8Bytes("keep-open"), GetUtf8Bytes("always"));
-            //_mpvRequestLogMessages(_mpvHandle, GetUtf8Bytes("warn"));
-
-
-            var intmpv = MpvInitialize(_mpvHandle);
-            Debug.WriteLine("Begin Play and init status {0}", intmpv, null);
-            DoMpvCommand("loadfile", _mediaFilePath);
-            Play();
         }
 
 
         partial void Pause(NSObject sender)
         {
-            Pause();
+            
         }
     }
 }
---
+++ MPVDemo/mpv.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Drawing;
+
+using static MPVDemo.libmpv;
+
+using System.Diagnostics;
+
+namespace MPVDemo
+{
+    public delegate void MpvBoolPropChangeHandler(string propName, bool value);
+
+    public class mpv
+    {
+        public  event Action Shutdown;
+        public  event Action AfterShutdown;
+        public  event Action FileLoaded;
+        public  event Action PlaybackRestart;
+        public  List<Action<bool>> BoolPropChangeActions = new List<Action<bool>>();
+        public  IntPtr MpvHandle;
+        public  IntPtr MpvWindowHandle;
+        public  string BR2 = Environment.NewLine + Environment.NewLine;
+
+        public mpv(IntPtr windowHandle)
+        {
+            MpvHandle = mpv_create();
+
+            MpvWindowHandle = windowHandle;
+
+            SetIntProp("input-ar-delay", 500);
+            SetIntProp("input-ar-rate", 20);
+            SetIntProp("volume", 50);
+            SetStringProp("hwdec", "auto");
+            SetStringProp("input-default-bindings", "yes");
+            //SetStringProp("opengl-backend", "angle");
+            SetStringProp("osd-playing-msg", "'${filename}'");
+            SetStringProp("profile", "opengl-hq");
+            SetStringProp("screenshot-directory", "~~desktop/");
+            SetStringProp("vo", "opengl");
+            SetStringProp("keep-open", "always");
+            SetStringProp("keep-open-pause", "no");
+            SetStringProp("osc", "yes");
+            SetStringProp("config", "yes");
+            SetStringProp("wid", MpvWindowHandle.ToString());
+            SetStringProp("force-window", "yes");
+            mpv_initialize(MpvHandle);
+            Task.Run(() => { EventLoop(); });
+
+        }
+
+        public void EventLoop()
+        {
+            while (true)
+            {
+                IntPtr ptr = mpv_wait_event(MpvHandle, -1);
+                mpv_event evt = (mpv_event)Marshal.PtrToStructure(ptr, typeof(mpv_event));
+                Debug.WriteLine(evt.event_id);
+
+                switch (evt.event_id)
+                {
+                    case mpv_event_id.MPV_EVENT_SHUTDOWN:
+                        Shutdown?.Invoke();
+                        AfterShutdown?.Invoke();
+                        return;
+                    case mpv_event_id.MPV_EVENT_FILE_LOADED:
+                        FileLoaded?.Invoke();
+                        break;
+                    case mpv_event_id.MPV_EVENT_PLAYBACK_RESTART:
+                        PlaybackRestart?.Invoke();
+                        var s = new Size(GetIntProp("dwidth"), GetIntProp("dheight"));
+
+
+                        break;
+                    case mpv_event_id.MPV_EVENT_PROPERTY_CHANGE:
+                        var eventData = (mpv_event_property)Marshal.PtrToStructure(evt.data, typeof(mpv_event_property));
+
+                        if (eventData.format == mpv_format.MPV_FORMAT_FLAG)
+                            foreach (var action in BoolPropChangeActions)
+                                action.Invoke(Marshal.PtrToStructure<int>(eventData.data) == 1);
+                        break;
+                }
+            }
+        }
+
+        public void Command(params string[] args)
+        {
+            if (MpvHandle == IntPtr.Zero)
+                return;
+
+            IntPtr[] byteArrayPointers;
+            var mainPtr = AllocateUtf8IntPtrArrayWithSentinel(args, out byteArrayPointers);
+            int err = mpv_command(MpvHandle, mainPtr);
+
+            if (err < 0)
+                throw new Exception($"{(mpv_error)err}");
+
+            foreach (var ptr in byteArrayPointers)
+                Marshal.FreeHGlobal(ptr);
+
+            Marshal.FreeHGlobal(mainPtr);
+        }
+
+        public void CommandString(string command, bool throwException = true)
+        {
+            if (MpvHandle == IntPtr.Zero)
+                return;
+
+            int err = mpv_command_string(MpvHandle, command);
+
+            if (err < 0 && throwException)
+                throw new Exception($"{(mpv_error)err}" + BR2 + command);
+        }
+
+        public void SetStringProp(string name, string value, bool throwException = true)
+        {
+            var bytes = GetUtf8Bytes(value);
+            int err = mpv_set_property(MpvHandle, GetUtf8Bytes(name), mpv_format.MPV_FORMAT_STRING, ref bytes);
+
+            if (err < 0 && throwException)
+                throw new Exception($"{name}: {(mpv_error)err}");
+        }
+
+        public string GetStringProp(string name)
+        {
+            var lpBuffer = IntPtr.Zero;
+            int err = mpv_get_property(MpvHandle, GetUtf8Bytes(name), mpv_format.MPV_FORMAT_STRING, ref lpBuffer);
+            var ret = StringFromNativeUtf8(lpBuffer);
+            mpv_free(lpBuffer);
+
+            if (err < 0)
+                throw new Exception($"{name}: {(mpv_error)err}");
+
+            return ret;
+        }
+
+        public int GetIntProp(string name, bool throwException = true)
+        {
+            var lpBuffer = IntPtr.Zero;
+            int err = mpv_get_property(MpvHandle, GetUtf8Bytes(name), mpv_format.MPV_FORMAT_INT64, ref lpBuffer);
+
+            if (err < 0 && throwException)
+                throw new Exception($"{name}: {(mpv_error)err}");
+            else
+                return lpBuffer.ToInt32();
+        }
+
+        public void SetIntProp(string name, int value)
+        {
+            Int64 val = value;
+            int err = mpv_set_property(MpvHandle, GetUtf8Bytes(name), mpv_format.MPV_FORMAT_INT64, ref val);
+
+            if (err < 0)
+                throw new Exception($"{name}: {(mpv_error)err}");
+        }
+
+        public void ObserveBoolProp(string name, Action<bool> action)
+        {
+            BoolPropChangeActions.Add(action);
+            int err = mpv_observe_property(MpvHandle, (ulong)action.GetHashCode(), name, mpv_format.MPV_FORMAT_FLAG);
+
+            if (err < 0)
+                throw new Exception($"{name}: {(mpv_error)err}");
+        }
+
+        public void UnobserveBoolProp(string name, Action<bool> action)
+        {
+            BoolPropChangeActions.Remove(action);
+            int err = mpv_unobserve_property(MpvHandle, (ulong)action.GetHashCode());
+
+            if (err < 0)
+                throw new Exception($"{name}: {(mpv_error)err}");
+        }
+
+        public void LoadFile(string file)
+        {
+            this.Command("loadfile", file);
+        }
+
+
+        public IntPtr AllocateUtf8IntPtrArrayWithSentinel(string[] arr, out IntPtr[] byteArrayPointers)
+        {
+            int numberOfStrings = arr.Length + 1; // add extra element for extra null pointer last (sentinel)
+            byteArrayPointers = new IntPtr[numberOfStrings];
+            IntPtr rootPointer = Marshal.AllocCoTaskMem(IntPtr.Size * numberOfStrings);
+
+            for (int index = 0; index < arr.Length; index++)
+            {
+                var bytes = GetUtf8Bytes(arr[index]);
+                IntPtr unmanagedPointer = Marshal.AllocHGlobal(bytes.Length);
+                Marshal.Copy(bytes, 0, unmanagedPointer, bytes.Length);
+                byteArrayPointers[index] = unmanagedPointer;
+            }
+
+            Marshal.Copy(byteArrayPointers, 0, rootPointer, numberOfStrings);
+            return rootPointer;
+        }
+
+        public string[] NativeUtf8StrArray2ManagedStrArray(IntPtr pUnmanagedStringArray, int StringCount)
+        {
+            IntPtr[] pIntPtrArray = new IntPtr[StringCount];
+            string[] ManagedStringArray = new string[StringCount];
+            Marshal.Copy(pUnmanagedStringArray, pIntPtrArray, 0, StringCount);
+
+            for (int i = 0; i < StringCount; i++)
+                ManagedStringArray[i] = StringFromNativeUtf8(pIntPtrArray[i]);
+
+            return ManagedStringArray;
+        }
+
+        public string StringFromNativeUtf8(IntPtr nativeUtf8)
+        {
+            int len = 0;
+            while (Marshal.ReadByte(nativeUtf8, len) != 0) ++len;
+            byte[] buffer = new byte[len];
+            Marshal.Copy(nativeUtf8, buffer, 0, buffer.Length);
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        public byte[] GetUtf8Bytes(string s) => Encoding.UTF8.GetBytes(s + "\0");
+    }
+}
