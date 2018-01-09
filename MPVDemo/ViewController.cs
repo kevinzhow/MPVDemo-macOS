d3a5f5237898f523403ddd7070e79f7f47b7189d
kevin <kevinchou.c@gmail.com>
2018-01-10 00:40:47 +0800

    Adbility to use FFmpeg to process video info

--- MPVDemo/ViewController.cs
+++ MPVDemo/ViewController.cs
@@ -58,8 +58,68 @@ namespace MPVDemo
                     _mediaFilePath = path;
                     Debug.WriteLine("We have url: {0}", path, null);
 
+                    ProcessWithFFmpeg(path);
                 }
             }
+        }
+
+        unsafe void ProcessWithFFmpeg(string path) {
+            // FFmpeg test
+
+            ffmpeg.av_log_set_level(ffmpeg.AV_LOG_VERBOSE);
+            av_log_set_callback_callback logCallback = (p0, level, format, vl) =>
+            {
+                if (level > ffmpeg.av_log_get_level()) return;
+
+                var lineSize = 1024;
+                var lineBuffer = stackalloc byte[lineSize];
+                var printPrefix = 1;
+                ffmpeg.av_log_format_line(p0, level, format, vl, lineBuffer, lineSize, &printPrefix);
+                var line = Marshal.PtrToStringAnsi((IntPtr)lineBuffer);
+                Console.Write(line);
+            };
+
+            ffmpeg.av_log_set_callback(logCallback);
+
+            var pFormatContext = ffmpeg.avformat_alloc_context();
+            int error;
+
+            error = ffmpeg.avformat_open_input(&pFormatContext, path, null, null);
+            if (error != 0) throw new ApplicationException(GetErrorMessage(error));
+
+            error = ffmpeg.avformat_find_stream_info(pFormatContext, null);
+            if (error != 0) throw new ApplicationException(GetErrorMessage(error));
+
+
+            AVDictionaryEntry* tag = null;
+            while ((tag = ffmpeg.av_dict_get(pFormatContext->metadata, "", tag, ffmpeg.AV_DICT_IGNORE_SUFFIX)) != null)
+            {
+                var key = Marshal.PtrToStringAnsi((IntPtr)tag->key);
+                var value = Marshal.PtrToStringAnsi((IntPtr)tag->value);
+                Console.WriteLine($"{key} = {value}");
+            }
+
+            AVStream* pStream = null;
+            for (var i = 0; i < pFormatContext->nb_streams; i++)
+                if (pFormatContext->streams[i]->codec->codec_type == AVMediaType.AVMEDIA_TYPE_VIDEO)
+                {
+                    pStream = pFormatContext->streams[i];
+                    break;
+                }
+            if (pStream == null) throw new ApplicationException(@"Could not found video stream.");
+
+            var codecContext = *pStream->codec;
+
+            Console.WriteLine($"codec name: {ffmpeg.avcodec_get_name(codecContext.codec_id)}");
+        }
+
+        private static unsafe string GetErrorMessage(int error)
+        {
+            var bufferSize = 1024;
+            var buffer = stackalloc byte[bufferSize];
+            ffmpeg.av_strerror(error, buffer, (ulong)bufferSize);
+            var message = Marshal.PtrToStringAnsi((IntPtr)buffer);
+            return message;
         }
 
         partial void Play(NSObject sender)
